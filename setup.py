b5e8004adaed394acfc908cea714169675667e45
Pradeep Kumar N <prajeet67@gmail.com>
2024-07-16 15:35:17 +0530

    Modified Desc

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="indian-repair",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[
         'click',  # Add click as a dependency
